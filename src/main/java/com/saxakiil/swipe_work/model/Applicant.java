b93fe776bd41e6d612b91a7e1c795a597f87dc30
saxakiil <vovakurguzov1999@yandex.ru>
2020-11-16 04:08:08 +0300

    Extend Entity class and add ApplicantController

--- src/main/java/com/saxakiil/swipe_work/model/Applicant.java
+++ src/main/java/com/saxakiil/swipe_work/model/Applicant.java
@@ -1,12 +1,17 @@
 package com.saxakiil.swipe_work.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 @Table(name="applicant")
 @Data
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Applicant {
 
     @Id
@@ -40,5 +45,28 @@ public class Applicant {
 
     @Column(name = "photo")
     private String photo;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "applicant_skills",
+            joinColumns = @JoinColumn(name = "id_applicant"),
+            inverseJoinColumns = @JoinColumn(name = "id_skill")
+    )
+    private Collection<Skills> skills;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "applicant_watched_vacations",
+            joinColumns = @JoinColumn(name = "applicant_id"),
+            inverseJoinColumns = @JoinColumn(name = "watched_vacations_id")
+    )
+    private Collection<Vacation> watchedVacations;
+
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "applicant_accept_vacations",
+            joinColumns = @JoinColumn(name = "applicant_id"),
+            inverseJoinColumns = @JoinColumn(name = "accept_vacations_id")
+    )
+    private Collection<Vacation> acceptVacations;
 
 }
--- src/main/java/com/saxakiil/swipe_work/rest/ApplicantController.java
+++ src/main/java/com/saxakiil/swipe_work/rest/ApplicantController.java
@@ -1,15 +1,22 @@
 package com.saxakiil.swipe_work.rest;
 
+import com.saxakiil.swipe_work.model.Applicant;
 import com.saxakiil.swipe_work.service.ApplicantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/applicants/")
+@RequestMapping("api/v1/applicants")
 public class ApplicantController {
 
     @Autowired
     private ApplicantService applicantService;
 
+    @GetMapping("/getApplicant")
+    public Applicant getApplicant(@RequestParam Long id) {
+        return applicantService.findById(id);
+    }
 }
